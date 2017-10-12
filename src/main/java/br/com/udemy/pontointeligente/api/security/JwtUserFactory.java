85318da4a349abd1bfbb38196d67e50499784810
Luis <luismelo.mg@gmail.com>
2017-10-12 17:35:39 -0300

    Adicao do pacote de seguranca e dependencias

---
+++ src/main/java/br/com/udemy/pontointeligente/api/security/JwtUserFactory.java
@@ -0,0 +1,45 @@
+package br.com.udemy.pontointeligente.api.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import br.com.udemy.pontointeligente.api.enumerate.Perfil;
+import br.com.udemy.pontointeligente.api.modelo.entity.Funcionario;
+
+
+public class JwtUserFactory {
+
+	private JwtUserFactory() {
+	}
+
+	/**
+	 * Converte e gera um JwtUser com base nos dados de um funcionário.
+	 * 
+	 * @param funcionario
+	 * @return JwtUser
+	 */
+	public static JwtUser create(Funcionario funcionario) {
+		return new JwtUser(funcionario.getId(), funcionario.getEmail(), funcionario.getSenha(),
+				mapToGrantedAuthorities(funcionario.getPerfil()));
+	}
+
+	/**
+	 * Converte o perfil do usuário para o formato utilizado pelo Spring Security.
+	 * 
+	 * @param perfil
+	 * @return List<GrantedAuthority>
+	 */
+	private static List<GrantedAuthority> mapToGrantedAuthorities(Perfil perfil) {
+		
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		
+		authorities.add(new SimpleGrantedAuthority(perfil.toString()));
+		
+		return authorities;
+		
+	}
+
+}
